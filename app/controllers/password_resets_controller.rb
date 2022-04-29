af493db4eb912163ff60d289fae08f48f8baddcc
Chris Wimbrow <cwimbrow@cubesmart.com>
2022-04-29 09:19:54 -0400

    21-22

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,23 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user = User.find_signed(params[:token], purpose: 'password_reset')
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Token has expired'
+  end
 
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    if user.update(password_params)
+      redirect_to sign_in_path, notice: 'Password changed'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
